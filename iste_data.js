df1bfbe216daa692573cbd3b9c6747d430aa08ca
Aviral Tripathi <aviralotrip@gmail.com>
2023-05-16 16:11:39 +0530

    added FAQs to form.html

--- iste_data.js
+++ iste_data.js
@@ -51,3 +51,24 @@ const saveMessages = (first_name, last_name, emailid, contact_number, address, m
 const getElementVal = (id) => {
     return document.getElementById(id).value;
 };
+const ourItemDiv = document.getElementsByClassName("item");
+const openIcon = document.getElementsByClassName("iconOpen");
+const closeIcon = document.getElementsByClassName("iconClose");
+
+// console.log(ourItemDiv);
+
+for (let i = 0; i < ourItemDiv.length; i++) {
+  closeIcon[i].style.display = "none";
+
+  ourItemDiv[i].addEventListener("click", () => {
+    const result = ourItemDiv[i].classList.toggle("active");
+
+    if (result) {
+      closeIcon[i].style.display = "block";
+      openIcon[i].style.display = "none";
+    } else {
+      closeIcon[i].style.display = "none";
+      openIcon[i].style.display = "block";
+    }
+  });
+}
